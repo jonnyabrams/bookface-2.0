035114ebf8244f62045d36850f006e1a61c3eb49
jonnyabrams <jonnyabrams@hotmail.com>
2022-11-04 15:34:51 +0000

    feat: Register page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
 import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 
 
 const App = () => {
   return (
     <div>
-      <Login />
+      <Register />
     </div>
   );
 }
---
+++ client/src/pages/register/Register.tsx
@@ -0,0 +1,35 @@
+import "./register.scss";
+
+const Register = () => {
+  return (
+    <div className="register">
+      <div className="card">
+        <div className="left">
+          <h1>BOOK FACE</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti
+            veritatis dolorem nulla eum odio tempore, asperiores molestias
+            repudiandae et natus nesciunt excepturi sit non sed, libero,
+            recusandae exercitationem aliquid vero.
+          </p>
+          <span>Already have an account?</span>
+          <button>Login</button>
+        </div>
+
+        <div className="right">
+          <h1>Register</h1>
+          <form>
+            <input type="text" placeholder="First name" />
+            <input type="text" placeholder="Last name" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <input type="password" placeholder="Confirm password" />
+            <button>Register</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
