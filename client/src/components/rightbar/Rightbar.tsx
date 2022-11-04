688c0e053e552ac84896b7e0ebe405d788211192
jonnyabrams <jonnyabrams@hotmail.com>
2022-11-04 18:46:51 +0000

    feat: Rightbar

--- client/src/components/rightbar/Rightbar.tsx
+++ client/src/components/rightbar/Rightbar.tsx
@@ -2,8 +2,108 @@ import "./rightbar.scss";
 
 const Rightbar = () => {
   return (
-    <div className="rightbar">Rightbar</div>
-  )
-}
+    <div className="rightbar">
+      <div className="container">
+        <div className="item">
+          <span>Suggestions For You</span>
+          <div className="user">
+            <div className="user-info">
+              <img src="/default-profile.jpeg" alt="" />
+              <span>Dwigt Rortugal</span>
+            </div>
 
-export default Rightbar
+            <div className="buttons">
+              <button>Follow</button>
+              <button>Dismiss</button>
+            </div>
+          </div>
+          <div className="user">
+            <div className="user-info">
+              <img src="/default-profile.jpeg" alt="" />
+              <span>Dwigt Rortugal</span>
+            </div>
+
+            <div className="buttons">
+              <button>Follow</button>
+              <button>Dismiss</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="item">
+          <span>Latest Activities</span>
+          <div className="user">
+            <div className="user-info">
+              <img src="/default-profile.jpeg" alt="" />
+              <p>
+                <span>Dwigt Rortugal</span> changed their cover picture
+              </p>
+            </div>
+            <span>1 minute ago</span>
+          </div>
+          <div className="user">
+            <div className="user-info">
+              <img src="/default-profile.jpeg" alt="" />
+              <p>
+                <span>Dwigt Rortugal</span> changed their cover picture
+              </p>
+            </div>
+            <span>1 minute ago</span>
+          </div>
+          <div className="user">
+            <div className="user-info">
+              <img src="/default-profile.jpeg" alt="" />
+              <p>
+                <span>Dwigt Rortugal</span> changed their cover picture
+              </p>
+            </div>
+            <span>1 minute ago</span>
+          </div>
+          <div className="user">
+            <div className="user-info">
+              <img src="/default-profile.jpeg" alt="" />
+              <p>
+                <span>Dwigt Rortugal</span> changed their cover picture
+              </p>
+            </div>
+            <span>1 minute ago</span>
+          </div>
+        </div>
+
+        <div className="item">
+          <span>Online Friends</span>
+          <div className="user">
+            <div className="user-info">
+              <img src="/default-profile.jpeg" alt="" />
+              <div className="online" />
+              <span>Dwigt Rortugal</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="user-info">
+              <img src="/default-profile.jpeg" alt="" />
+              <div className="online" />
+              <span>Dwigt Rortugal</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="user-info">
+              <img src="/default-profile.jpeg" alt="" />
+              <div className="online" />
+              <span>Dwigt Rortugal</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="user-info">
+              <img src="/default-profile.jpeg" alt="" />
+              <div className="online" />
+              <span>Dwigt Rortugal</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Rightbar;
