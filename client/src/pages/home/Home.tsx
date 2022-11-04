984cfada9b443c31329b92e47af3afea4a264a6a
jonnyabrams <jonnyabrams@hotmail.com>
2022-11-04 17:24:48 +0000

    styles: fix Leftbar overflow

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,9 +1,11 @@
 import "./home.scss";
 
 const Home = () => {
-  return <div className="home">
-    <h1>Home</h1>
-  </div>;
+  return (
+    <div className="home">
+      <h1>Home</h1>
+    </div>
+  );
 };
 
 export default Home;
