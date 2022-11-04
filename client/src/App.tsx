9d2d14abb1a03fadcc2d2be3a1cf542c97fe0fbd
jonnyabrams <jonnyabrams@hotmail.com>
2022-11-04 15:50:08 +0000

    feat: Layout (Home & Profile)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,52 @@
-import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Route, Outlet } from "react-router-dom";
+import Leftbar from "./components/leftbar/Leftbar";
+import Navbar from "./components/navbar/Navbar";
+import Rightbar from "./components/rightbar/Rightbar";
+import Home from "./pages/home/Home";
 
 import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
 import Register from "./pages/register/Register";
 
-const router = createBrowserRouter([
-  {
-    path: "/login",
-    element: <Login />
-  },
-  {
-    path: "/register",
-    element: <Register />
-  }
-])
-
 const App = () => {
+  const Layout = () => {
+    return (
+      <div>
+        <Navbar />
+        <div style={{ display: "flex" }}>
+          <Leftbar />
+          <Outlet />
+          <Rightbar />
+        </div>
+      </div>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />
+        },
+        {
+          path: "/profile/:id",
+          element: <Profile />
+        },
+      ]
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+  ]);
+
   return (
     <div>
       <RouterProvider router={router} />
