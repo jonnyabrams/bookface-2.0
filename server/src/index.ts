977bd3df0441eb42194762a63225b2050f0d8bea
jonnyabrams <jonnyabrams@hotmail.com>
2022-11-10 11:21:58 +0000

    feat: import routes in index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,21 @@
-import express from "express"
+import express from "express";
 
-import userRoutes from "./routes/userRoutes"
+import userRoutes from "./routes/userRoutes";
+import postRoutes from "./routes/postRoutes";
+import commentRoutes from "./routes/commentRoutes";
+import likeRoutes from "./routes/likeRoutes";
+import authRoutes from "./routes/authRoutes";
 
 const app = express();
 
-app.use('/api/users', userRoutes)
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/like", likeRoutes);
+app.use("/api/auth", authRoutes);
 
-const port = 8000
+const port = 8000;
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
-})
+  console.log(`Listening on port ${port}`);
+});
