de8d3ff094ea8d09b737478fe77b14c89545bb6c
jonnyabrams <jonnyabrams@hotmail.com>
2022-11-11 09:52:34 +0000

    feat: getPosts query returns timeline posts

--- server/src/controllers/post.ts
+++ server/src/controllers/post.ts
@@ -1,19 +1,34 @@
 import { Request, Response } from "express";
+import jwt from "jsonwebtoken"
 
 import db from "../connect";
 
 // want user's own posts & friends' posts
 export const getPosts = async (req: Request, res: Response) => {
-  try {
-    const posts = await db.query(
-      "SELECT p.*, u.id AS user_id, first_name, last_name, profile_pic FROM posts AS p JOIN users AS u ON (u.id = p.user_id)"
-    );
+  // get token
+  const token = req.cookies.accessToken;
 
-    res.status(200).json(posts);
-  } catch (error) {
-    res.status(500).json(error);
-    console.log(error);
-  }
+  if (!token) return res.status(401).json("Not logged in")
+
+  jwt.verify(token, "secretkey", async (err: any, userInfo: any) => {
+    if (err) return res.status(403).json("Token invalid")
+
+    try {
+      const posts = await db.query(
+        // returns all user's posts + posts of those they follow
+        `SELECT p.*, u.id AS user_id, first_name, last_name, profile_pic FROM posts AS p JOIN users AS u ON (u.id = p.user_id)
+        LEFT JOIN follows AS f ON (p.user_id = f.followed_user_id) WHERE f.follower_user_id = $1 OR p.user_id = $1`, [userInfo.id]
+      );
+  
+      res.status(200).json(posts);
+    } catch (error) {
+      res.status(500).json(error);
+      console.log(error);
+    }
+
+  })
+
+ 
 };
 
 export const addPost = async (req: Request, res: Response) => {
